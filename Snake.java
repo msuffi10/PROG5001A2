e32bcbae322dd494aa774152f7141626317839e7
unknown <ali@gmail.com>
2021-04-25 13:53:34 +0500

    settting up repository

---
+++ Snake.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Snake here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Snake
+{
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake() {
+        // initialise instance variables        
+    }
+
+}
