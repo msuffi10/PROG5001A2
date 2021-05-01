d112cf8183ae5cb2812292e180f650296d342272
msuffi10 <82994953+msuffi10@users.noreply.github.com>
2021-05-01 22:36:39 +0500

    class changed to abstract
    i have add new abstract methods in this class.

--- MS_GameBoard.java
+++ MS_GameBoard.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class MS_GameBoard extends JPanel {    
+abstract class MS_GameBoard extends JPanel {    
     int xcells = 30;    
     int ycells = 30;
     int szcell = 10;       
@@ -18,5 +18,35 @@ public class MS_GameBoard extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
     }
+    
+    /**
+     * Method boardvisible
+     * this method is used to visible the board on screen
+     */
+    abstract void boardvisible();
+    
+    /**
+     * Method boardminimize
+     * this method is used to minimized the board
+     */
+    abstract void boardminimize();
+    
+    /**
+     * Method boardmaximize
+     * this method is used to maximized the board
+     */
+    abstract void boardmaximize();
+    
+    /**
+     * Method boardresize
+     * this method is used to resize the board
+     */
+    abstract void boardresize();
+    
+    /**
+     * Method boardclose
+     * this method is used to close the board
+     */
+    abstract void boardclose();
     
 }
