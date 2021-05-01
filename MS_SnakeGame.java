ddcf17d321ad573179c1f60df4199aeb9c2cfd01
msuffi10 <82994953+msuffi10@users.noreply.github.com>
2021-05-01 20:57:50 +0500

    Class name changed

--- MS_SnakeGame.java
+++ MS_SnakeGame.java
@@ -1,9 +1,9 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-public class SnakeGame extends JFrame {
+public class MS_SnakeGame extends JFrame {
 
-    public SnakeGame(String gameTille) {        
+    public MS_SnakeGame (String gameTille) {        
         setTitle(gameTille);
         setResizable(false);
         setLocationRelativeTo(null);
@@ -14,7 +14,7 @@ public class SnakeGame extends JFrame {
     public static void main(String[] args) {        
         EventQueue.invokeLater(() -> {
             //create the game with a game title as follow: The Snake Game (C) Your_Name
-            JFrame sgame = new SnakeGame("My Game");
+            JFrame sgame = new MS_SnakeGame ("My Game");
             sgame.setVisible(true);
         });
     }
