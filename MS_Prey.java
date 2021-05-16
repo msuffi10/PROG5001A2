58d8d976be035969b922ce1cad59efe18200ecf2
msuffi10 <82994953+msuffi10@users.noreply.github.com>
2021-05-16 14:21:04 +0500

    prey updated
    added locateapple and checkapple method in prey

--- MS_Prey.java
+++ MS_Prey.java
@@ -25,7 +25,7 @@ public class MS_Prey extends JPanel {
      */
     public MS_Prey()
     {
-        prey_image = new ImageIcon("resources/dot.png").getImage();
+        prey_image = new ImageIcon("images/smile.png").getImage();
     }  
     
     public void locateApple(int RAND_POS, int DOT_SIZE) {
